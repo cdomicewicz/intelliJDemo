b879101638d418a8be09fbbeb130867451c7b89e
cezary.domicewicz <cezary.domicewicz@sebn.com>
2022-06-08 10:32:32 +0200

    Add new featureX - display

--- src/org/lauchcode/Main.java
+++ src/org/lauchcode/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("New Branch");
+        System.out.println("featureX branch enabled");
     }
 }
