efaca1c4cd04efcfb0a91e16d8b0eb2ea6c70c80
cezary.domicewicz <cezary.domicewicz@sebn.com>
2022-06-08 14:52:55 +0200

    Add new class MyExample

---
+++ src/org/lauchcode/MyExample.java
@@ -0,0 +1,14 @@
+package org.lauchcode;
+
+public class MyExample {
+    private String author;
+
+    public MyExample(String author) {
+        this.author = author;
+    }
+
+    public void showAuthor() {
+        System.out.println(this.author);
+    }
+
+}
